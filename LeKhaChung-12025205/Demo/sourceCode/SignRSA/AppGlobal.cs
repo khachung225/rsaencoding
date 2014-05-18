6cde0891daf9a07aeda58cab3d58460e4a99813b
khachung225@gmail.com <khachung225@gmail.com>
2014-05-18 16:33:40 +0700

    HOAN THIEN A

---
+++ LeKhaChung-12025205/Demo/sourceCode/SignRSA/AppGlobal.cs
@@ -0,0 +1,22 @@
+﻿namespace SignRSA
+{
+    public class AppGlobal
+    {
+        public static int P{ get; set; }
+        public static int Q{ get; set; }
+        public static int N{ get; set; }
+        public static int E{ get; set; }
+        public static int D{ get; set; }
+        public static int PhiN{ get; set; }
+
+        public static string NoiDung { get; set; }
+
+        public static string ChuKy { get; set; }
+
+
+        public static string NoiDungnguyon { get; set; }
+
+        public static string ChuKynguoi { get; set; }
+
+    }
+}
---
+++ LeKhaChung-12025205/Demo/sourceCode/SignRSA/KiemTraChuKy.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SignRSA
+{
+    public partial class KiemTraChuKy : Form
+    {
+        private RSAAlgorithm _rsaAlgorithm = new RSAAlgorithm();
+        public KiemTraChuKy()
+        {
+            InitializeComponent();
+        }
+
+        private void btnnhannoidung_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                txtChuKy.Text = AppGlobal.ChuKy;
+                txtNoiDung.Text = AppGlobal.NoiDung;
+                txte.Text = AppGlobal.E.ToString(CultureInfo.InvariantCulture);
+                txtn.Text = AppGlobal.N.ToString(CultureInfo.InvariantCulture);
+            }
+            catch (Exception)
+            {
+                
+               
+            }
+
+        }
+
+        private void btnGiaiMa_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                txtnoidunggoc.Text = _rsaAlgorithm.GiaiMaBase64NoiDung(txtNoiDung.Text);
+               
+                txtchukygoc.Text = _rsaAlgorithm.GiaiMaBase64ChuKy(txtChuKy.Text); ;
+                //lblKetQua.Text = magia;
+            }
+            catch (Exception)
+            {
+                
+            }
+        }
+
+        private void btnkiemtra_Click(object sender, EventArgs e)
+        {
+          var isOK=  _rsaAlgorithm.Kiemtra(txtnoidunggoc.Text, txtChuKy.Text, AppGlobal.N, AppGlobal.D);
+            lblKetQua.Text = isOK ? "Xác nhận" : "Không xác nhận";
+        }
+
+    }
+}
